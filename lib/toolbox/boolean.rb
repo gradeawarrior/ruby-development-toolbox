e419b866b0a6df76ed8e073b616e600199c189f1
Peter Salas <psalas@proofpoint.com>
2014-02-20 13:13:16 -0800

    Better class documentation for hopefully easier reading on Yard

--- lib/toolbox/boolean.rb
+++ lib/toolbox/boolean.rb
@@ -1,5 +1,6 @@
 ##
-# Extending String function to be able to convert a String 'true' or 'false' to a boolean true/false
+# Extending String function to be able to convert a String 'true' or 'false' to a boolean true/false.
+# See the Boolean module for usage
 #
 class String
   def to_bool
@@ -8,7 +9,8 @@ class String
 end
 
 ##
-# Implementing to_bool operation that returns self
+# Implementing to_bool operation that returns self.
+# See the Boolean module for usage
 #
 class TrueClass
   def to_bool
@@ -17,7 +19,8 @@ class TrueClass
 end
 
 ##
-# Implementing to_bool operation that returns self
+# Implementing to_bool operation that returns self.
+# See the Boolean module for usage
 #
 class FalseClass
   def to_bool
@@ -26,10 +29,42 @@ class FalseClass
 end
 
 ##
-# Implementing to_bool operation that returns self
+# Implementing to_bool operation that returns self.
+# See the Boolean module for usage
 #
 class NilClass
   def to_bool
     self
   end
+end
+
+##
+# The main application of the Boolean module is to
+# support reading boolean values from a String (e.g.
+# while reading a configuration value) and having the
+# ability to convert it back to a boolean true/false
+# for easier evaluation in your Ruby code
+#
+# == Usage
+#
+# Working with Boolean module can be very simple, for example:
+#
+#     require 'toolbox/boolean'
+#
+#     list_of_values = [
+#       'true',
+#       'True',
+#       'TRUE',
+#       'false',
+#       'False',
+#       'FALSE',
+#       nil,
+#     ]
+#
+#     list_of_values.each do |string|
+#       puts "This evaluated to true" if string.to_bool
+#       puts "This evaluated to false or was nil" unless string.to_bool
+#     end
+#
+module Boolean
 end
