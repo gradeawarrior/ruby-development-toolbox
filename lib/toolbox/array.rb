98ca48d2e70883dbcef977e803be21c691e36d21
Peter Salas <psalas@proofpoint.com>
2014-12-22 11:38:00 -0800

    Hopefully making toolbox/array easier to read in rubydoc

--- lib/toolbox/array.rb
+++ lib/toolbox/array.rb
@@ -23,17 +23,17 @@ module Toolbox
   # we use =~ as a means of checking if array1 is contained in array2; or if
   # object is contained in array1
   #
-  #   sample_array = [1, 2, 3]
-  #   puts sample_array =~ 3    ## This prints true
-  #   puts sample_array =~ 4    ## This prints false
+  #     sample_array = [1, 2, 3]
+  #     puts sample_array =~ 3    ## This prints true
+  #     puts sample_array =~ 4    ## This prints false
   #
   # The other way is the following:
   #
-  #   sample_array1 = [1, 2, 3]
-  #   sample_array2 = [1, 2, 3, 4]
-  #
-  #   puts sample_array2 =~ sample_array1   ## This prints true, because everything in array1 is in array2
-  #   puts sample_array1 =~ sample_array2   ## This prints false, because not everything in array2 is in array1
+  #     sample_array1 = [1, 2, 3]
+  #     sample_array2 = [1, 2, 3, 4]
+  #     
+  #     puts sample_array2 =~ sample_array1   ## This prints true, because everything in array1 is in array2
+  #     puts sample_array1 =~ sample_array2   ## This prints false, because not everything in array2 is in array1
   #
   module Array
   end
